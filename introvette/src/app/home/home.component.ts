6b649e1cca254ae101e0bd989db2fec73dc6e171
Yvette Rosario <vette.rosario@gmail.com>
2023-04-24 22:31:26 -0400

    all pages mobile responsive

--- introvette/src/app/home/home.component.ts
+++ introvette/src/app/home/home.component.ts
@@ -1,23 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  slides: string [] = ['./assets/img/me.png', './assets/img/pollo.png' ]
-  i=0;
+export class HomeComponent implements OnInit {
+  slides = [
+    {
+      imageUrl: 'assets/img/pic5.JPG',
+      title: 'Slide 1',
+      description: 'This is the description for Slide 1.'
+    },
+    {
+      imageUrl: 'assets/img/pic4.JPG',
+      title: 'Slide 2',
+      description: 'This is the description for Slide 2.'
+    },
+    {
+      imageUrl: 'assets/img/pic3.JPEG',
+      title: 'Slide 3',
+      description: 'This is the description for Slide 3.'
+    },
+    {
+      imageUrl: 'assets/img/pic2.JPG',
+      title: 'Slide 4',
+      description: 'This is the description for Slide 4.'
+    },
+    {
+      imageUrl: 'assets/img/pic1.JPG',
+      title: 'Slide 5',
+      description: 'This is the description for Slide 5.'
+    }
+  ];
 
-  getSlide() {
-      return this.slides[this.i];
+  currentIndex = 0;
+
+  ngOnInit() {
+
   }
 
-  getPrev() {
-      this.i = this.i===0 ? 0 : this.i - 1;
+  nextSlide() {
+    console.log('Next slide clicked');
+    console.log('Current index before update:', this.currentIndex);
+    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+    console.log('Current index after update:', this.currentIndex);
   }
-//edit here
-  getNext() {
-      this.i = this.i===this.slides.length ? this.i : this.i + 1;
+
+
+  prevSlide() {
+    console.log('Prev slide clicked');
+    console.log('Current index before update:', this.currentIndex);
+    this.currentIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+    console.log('Current index after update:', this.currentIndex);
   }
 }
