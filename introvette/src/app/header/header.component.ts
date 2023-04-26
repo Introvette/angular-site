d41d354f1fbea21b37af84d99a4ca16763774d03
Yvette Rosario <vette.rosario@gmail.com>
2023-04-26 13:39:35 -0400

    styling

--- introvette/src/app/header/header.component.ts
+++ introvette/src/app/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isDropdownOpen = false;
 
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
 }
